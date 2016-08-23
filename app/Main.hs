a1657ed03936472aaf8b347b3c1246c6b85b05b0
Hogeyama <gan13027830@gmail.com>
2016-08-23 21:30:09 +0900

    implement app/Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,58 @@
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Main where
 
-import AllTypes
-import Type
-import Lexer
-import Parser
-import Typing
-import KNormal
-import System.IO (Handle, stdin, hGetContents, openFile, IOMode(..))
+import AllTypes (runCaml, initialState, Error, S(..))
+import Lexer    (lex)
+import Parser   (parse)
+import Typing   (typing)
+import KNormal  (kNormalize)
+import Alpha    (alpha)
+import Optimise (optimise)
+import Closure  (closureConvert)
+import Virtual  (virtualCode)
+import RegAlloc (regAlloc)
+import Simm     (simm)
+import Emit     (emit)
+
+import Prelude hiding (lex)
+import Control.Monad (forM_, when)
+import System.IO (readFile, withFile, Handle, IOMode(..))
+import Options
 
 main :: IO ()
-main = do
-  print $ Just $ Just $ Just $ Just 1
+main = runCommand $ \opts args -> do
+  let s = initialState { _threshold = inline opts
+                       , _optimiseLimit = iter opts}
+  mapM_ (file s) args
+
+data MinCamlOptions = MinCamlOptions
+                    { inline :: Int
+                    , iter   :: Int
+                    }
+instance Options MinCamlOptions where
+  defineOptions = pure MinCamlOptions
+               <*> simpleOption "inline"
+                   0 "maximum size of functions inlined"
+               <*> simpleOption "iter"
+                   100 "maximum number of optimizations iterated"
+
+file :: S -> FilePath -> IO ()
+file s f = do
+  str <- readFile (f ++ ".ml")
+  withFile (f ++ "-res.s") WriteMode $ \out -> do
+    m <-(`runCaml` s) $ lex str
+          >>= parse
+          >>= typing
+          >>= kNormalize
+          >>= alpha
+          >>= optimise
+          >>= closureConvert
+          >>= virtualCode
+          >>= simm
+          >>= regAlloc
+          >>= emit out
+    case m of
+      Right () -> return ()
+      Left e -> error $ f ++ ": " ++ show e
+
